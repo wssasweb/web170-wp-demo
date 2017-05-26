bcc7b984f7c27cf7da323bd23758c60bbe3227a3
Sylvia <wssasweb@users.noreply.github.com>
2017-05-26 10:44:06 -0700

    Custom Posts Feed
    5/26 due assignment to Create a Custom Posts Feed with Featured Images for Your Site

--- index.php
+++ index.php
@@ -7,8 +7,14 @@
 
 <!--  Wordpress format for same php -->
 <?php if (have_posts()) : while (have_posts()) : the_post();   ?>
+	<article id="post-excerpt-<?php the_ID(); ?>" class="post-excerpt" >
 		<h2><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h2>
-		<?php the_content(); ?>
+		<small>Posted on the <?php the_time('F j, Y'); ?> by <?php the_author(); ?> 
+			in <?php the_category(', '); ?>.</small>
+		<?php the_post_thumbnail( 'thumbnail' ); ?>
+		<p><?php echo get_the_excerpt(); ?> 
+			<a href="<?php the_permalink(); ?>">Read More &nbsp;&raquo;</a></p>
+	</article>
 <?php endwhile; endif; ?>
 
 <small>index.php</small>
--- single.php
+++ single.php
@@ -8,8 +8,8 @@
 <?php if (have_posts()) : while (have_posts()) : the_post();   ?>
 	<article id="article-<?php the_ID(); ?> class="article">
 		<h2><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h2>
-		<small>Posted on the <?php the_time('F jS, Y'); ?> in the 
-				<?php the_category(); ?></small>
+		<small>Posted on the <?php the_time('F j, Y'); ?> by <?php the_author(); ?>
+				in the <?php the_category(', '); ?></small>
 		<?php the_post_thumbnail('large'); ?>
 		<?php the_content(); ?>
 	</article>
